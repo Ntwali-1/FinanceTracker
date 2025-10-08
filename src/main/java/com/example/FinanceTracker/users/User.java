d8b4adc25f85745e3cbdc2af1219aa83d2722668
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-08 07:58:37 +0200

    feat: UserEntity

--- src/main/java/com/example/FinanceTracker/users/User.java
+++ src/main/java/com/example/FinanceTracker/users/User.java
@@ -1,5 +1,20 @@
 package com.example.FinanceTracker.users;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    private String username;
+    private String password;
+    private String confirmPassword;
 }
