4889670bf74da56487e72e71fed0b28c679c2eba
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-09 21:50:21 +0200

    fixes: AppProperties

--- src/main/java/com/example/FinanceTracker/security/JwtService.java
+++ src/main/java/com/example/FinanceTracker/security/JwtService.java
@@ -2,18 +2,22 @@ package com.example.FinanceTracker.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${application.security.jwt.secret-key}")
+    private String secret;
+
     public String generateToken(String email){
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact()
         ;
     }
