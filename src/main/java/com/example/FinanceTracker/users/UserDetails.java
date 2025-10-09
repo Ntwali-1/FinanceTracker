626ecd4daace5cf989cedb12267badc021972b1c
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-09 19:36:00 +0200

    fix: UserDetails

--- src/main/java/com/example/FinanceTracker/users/UserDetails.java
+++ src/main/java/com/example/FinanceTracker/users/UserDetails.java
@@ -1,12 +1,30 @@
 package com.example.FinanceTracker.users;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
+@AllArgsConstructor
+@Service
 public class UserDetails implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
     @Override
-    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        try {
+            var user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+            return new User(
+                    user.getEmail(),
+                    user.getPassword(),
+                    Collections.emptyList()
+            );
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 }
