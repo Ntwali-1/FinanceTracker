1d1950c75c730866a41cf855b790a66a2d4c80e4
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-09 19:10:50 +0200

    feat: UserDetails

---
+++ src/main/java/com/example/FinanceTracker/users/UserDetails.java
@@ -0,0 +1,12 @@
+package com.example.FinanceTracker.users;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetails implements UserDetailsService {
+
+    @Override
+    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
