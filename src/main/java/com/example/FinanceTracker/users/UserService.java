53593b1f1632591aa0ce9f4f43309532821a77b7
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-09 13:56:05 +0200

    fix: UserController

--- src/main/java/com/example/FinanceTracker/users/UserService.java
+++ src/main/java/com/example/FinanceTracker/users/UserService.java
@@ -3,23 +3,35 @@ package com.example.FinanceTracker.users;
 import com.example.FinanceTracker.users.dto.SignupRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public ResponseEntity<User> create(SignupRequest signupRequest) {
         User user = new User();
         user.setEmail(signupRequest.getEmail());
-        user.setPassword(signupRequest.getPassword());
-        user.setConfirmPassword(signupRequest.getConfirmPassword());
 
-        User savedUser = userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+        if(!signupRequest.getPassword().equals(signupRequest.getConfirmPassword())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+
+        try{
+            userRepository.save(user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
